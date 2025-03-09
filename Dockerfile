34db4620b6c831000687722ca146304e1744db4c
Huang Jiahao <40518249+asd-a@users.noreply.github.com>
2025-03-09 15:16:21 +0800

    dev: add demo server & pack to docker (#4)
    * dev: demo server
    
    * dev: pack to docker
    
    * fix: code quality
    
    * fix: docker security
    
    * dev: build api with makefile & change build image to rust image
    
    * fix: remove unnecessary deps
    
    * feat(docker): add image version
    
    * feat(status_code): add status code to error response
    
    * feat(docker): use curruent commit to build image
    
    * fix(docker): build issues & security hotspots
    
    - remove recursively copying context directories
    - fix debian image tag
    - fix user missing issue
    - fix path traversal issue
    
    * fix(docker): sepecify path pattern
    
    * fix(docker): remove glob pattern
    
    * dep(api): bump api version

---
+++ Dockerfile
@@ -0,0 +1,27 @@
+
+# Use the latest stable version of Rust as the base image to build the project
+
+FROM rust:1.85-bookworm AS build
+WORKDIR /opt/back-end
+
+# Clone the repository
+
+COPY ./Cargo.toml ./Cargo.lock ./.gitmodules ./
+COPY ./src ./src
+COPY ./.git ./.git
+RUN git submodule update --init --recursive
+
+# Build the project
+
+RUN make -C api prepare
+RUN make -C api src/lib.rs
+RUN cargo build --release
+
+# Create a new image with only the necessary files to run the application
+
+FROM debian:bookworm AS runtime
+RUN groupadd -r back-end && useradd -r -g back-end back-end
+USER back-end
+EXPOSE 3000
+COPY --from=build /opt/back-end/target/release/back-end /back-end
+ENTRYPOINT [ "/back-end" ]
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,53 @@
-fn main() {
-    let unused = 1;
-    println!("Hello, world!");
+use api::{APICollection, Error, User, API};
+use axum::{
+    extract::State,
+    http::StatusCode,
+    response::{IntoResponse, Response},
+    routing::post,
+    Json, Router,
+};
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+
+#[derive(Serialize, Deserialize, Clone)]
+struct App {
+    db: HashMap<String, String>,
+}
+
+impl API for App {
+    async fn login(&mut self, _req: api::LoginRequest) -> Result<api::LoginResponse, api::Error> {
+        Err(Error {
+            code: 400_u16,
+            message: "bad request".to_string(),
+        })
+    }
+    async fn register(&mut self, _req: api::RegisterRequest) -> Result<api::LoginResponse, Error> {
+        Ok(api::LoginResponse {
+            user: User {
+                id: 1,
+                name: "rnoob".to_string(),
+                roles: vec![api::Role::user],
+            },
+            token: "token".to_string(),
+        })
+    }
+}
+
+async fn handler(
+    State(mut app): State<App>,
+    Json(body): Json<APICollection>,
+) -> Result<Json<impl Serialize>, Response> {
+    Ok(Json(app.handle(body).await.map_err(|e| {
+        (StatusCode::from_u16(e.code).unwrap(), Json(e)).into_response()
+    })?))
+}
+
+#[tokio::main]
+async fn main() {
+    let app = App { db: HashMap::new() };
+    // build our application with a single route
+    let router = Router::new().route("/", post(handler)).with_state(app);
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, router).await.unwrap();
 }
