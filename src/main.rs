ff3160e708f666c512b32c9489d8ac1fadeca461
asd-a <asdro.huang@outlook.com>
2025-03-26 16:47:45 +0800

    feat(api): bump 0.2.0-alpha

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,5 @@
-use api::{APICollection, Error, API};
-use axum::{
-    extract::State,
-    http::StatusCode,
-    response::{IntoResponse, Response},
-    routing::post,
-    Json, Router,
-};
+use api::{APICollection, API};
+use axum::{extract::State, response::Response, routing::post, Json, Router};
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use tower_http::cors::CorsLayer;
@@ -15,58 +9,13 @@ struct App {
     db: HashMap<String, String>,
 }
 
-impl API for App {
-    async fn login(&mut self, _req: api::LoginRequest) -> Result<api::LoginResponse, api::Error> {
-        todo!()
-    }
-    async fn register(&mut self, _req: api::RegisterRequest) -> Result<api::LoginResponse, Error> {
-        todo!()
-    }
-
-    async fn validate(&self, _role: api::Role, _auth: api::Auth) -> Result<api::Auth, Error> {
-        todo!()
-    }
-
-    async fn spare_return(
-        &mut self,
-        _req: api::SpareReturnRequest,
-        _auth: api::Auth,
-    ) -> Result<api::SpareReturnResponse, Error> {
-        todo!()
-    }
-
-    async fn spare_take(
-        &mut self,
-        _req: api::SpareTakeRequest,
-        _auth: api::Auth,
-    ) -> Result<api::SpareTakeResponse, Error> {
-        todo!()
-    }
-
-    async fn spare_list(
-        &mut self,
-        _req: api::SpareListRequest,
-        _auth: api::Auth,
-    ) -> Result<api::SpareListResponse, Error> {
-        todo!()
-    }
-
-    async fn test_auth_echo(
-        &mut self,
-        _req: api::TestAuthEchoRequest,
-        _auth: api::Auth,
-    ) -> Result<api::TestAuthEchoResponse, Error> {
-        todo!()
-    }
-}
+impl API for App {}
 
 async fn handler(
     State(mut app): State<App>,
     Json(body): Json<APICollection>,
 ) -> Result<Json<impl Serialize>, Response> {
-    Ok(Json(app.handle(body).await.map_err(|e| {
-        (StatusCode::from_u16(e.code).unwrap(), Json(e)).into_response()
-    })?))
+    Ok(Json(app.handle(body).await))
 }
 
 #[tokio::main]
