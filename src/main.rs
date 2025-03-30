1739fe6fa577ae23327fe894e6db6839c4bc254e
Asdro Huang <40518249+asd-a@users.noreply.github.com>
2025-03-30 12:54:56 +0800

    dev(sql): setup database & sqlx support (#16)
    * dev(sql): config db & sql migration
    
    * upd(docker): sql migration support

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 use api::{APICollection, API};
 use axum::{extract::State, response::Response, routing::post, Json, Router};
-use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
+use serde::Serialize;
+use sqlx::{
+    migrate::MigrateDatabase, Sqlite, SqlitePool,
+};
 use tower_http::cors::CorsLayer;
-
-#[derive(Serialize, Deserialize, Clone)]
+const DATABASE_URL: &str = "sqlite://sqlite.db";
+#[derive(Clone)]
 struct App {
-    db: HashMap<String, String>,
+    pool: SqlitePool,
 }
 
 impl API for App {}
@@ -15,12 +17,26 @@ async fn handler(
     State(mut app): State<App>,
     Json(body): Json<APICollection>,
 ) -> Result<Json<impl Serialize>, Response> {
+    let _conn = app.pool.acquire().await;
     Ok(Json(app.handle(body).await))
 }
 
 #[tokio::main]
-async fn main() {
-    let app = App { db: HashMap::new() };
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tracing_subscriber::fmt().init();
+
+    // Create a new SQLite database if it doesn't exist
+    if !Sqlite::database_exists(DATABASE_URL).await? {
+        Sqlite::create_database(DATABASE_URL).await?;
+    }
+
+    // Create a new SQLite connection pool
+    let pool = SqlitePool::connect(DATABASE_URL).await?;
+    
+    // Run migrations
+    sqlx::migrate!().run(&pool).await?;
+
+    let app = App { pool };
     // cors allow all
     let cors = CorsLayer::permissive();
     // build our application with a single route
@@ -31,4 +47,5 @@ async fn main() {
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:80").await.unwrap();
     axum::serve(listener, router).await.unwrap();
+    Ok(())
 }
