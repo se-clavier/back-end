579744677c29d4457b04fee58116e852c0635de9
asd-a <asdro.huang@outlook.com>
2025-03-31 22:06:26 +0800

    feat(hash): add password hash

--- src/app/fixtures/users.sql
+++ src/app/fixtures/users.sql
@@ -1,4 +1,4 @@
 -- This file is used to insert test data into the table users and user_roles.
 
-insert into users (username, password) values ("testuser", "password123");
+insert into users (username, password) values ("testuser", "$argon2id$v=19$m=19456,t=2,p=1$YmFzZXNhbHQ$3i0y+gypIZM5SX0/X2fkMRZI8fXiw6PP/5/JLHj+Tpg"); -- password: password123
 insert into user_roles (user_id, role_type) values (1, "user");
