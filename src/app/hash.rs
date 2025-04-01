220d8de852c6f27c351fef234ad301f93339cdbb
asd-a <asdro.huang@outlook.com>
2025-04-01 10:25:27 +0800

    perf: store `Argon2` structure in `Hasher`

--- src/app/hash.rs
+++ src/app/hash.rs
@@ -3,33 +3,33 @@ use argon2::{password_hash::SaltString, Argon2, PasswordHash, PasswordHasher, Pa
 #[derive(Debug, Clone)]
 pub struct Hasher {
     salt: SaltString,
+    argon2: Argon2<'static>,
 }
 
 impl Hasher {
     pub fn new(salt: &str) -> Self {
         Self {
             salt: SaltString::from_b64(salt).unwrap(),
+            argon2: Argon2::default(),
         }
     }
 
     pub fn hash(&self, password: &str) -> String {
-        let argon2 = Argon2::default();
-        argon2
+        self.argon2
             .hash_password(password.as_bytes(), &self.salt)
             .unwrap()
             .to_string()
     }
 
     pub fn verify(&self, password: &str, hash: &str) -> bool {
-        let argon2 = Argon2::default();
-        argon2
+        self.argon2
             .verify_password(password.as_bytes(), &PasswordHash::new(hash).unwrap())
             .is_ok()
     }
 }
 
 #[cfg(test)]
-mod test{
+mod test {
     use super::*;
 
     #[test]
