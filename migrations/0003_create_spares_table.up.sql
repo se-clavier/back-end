60b7a286c339e7194224d3d6946e2f56854ea8b1
asd-a <asdro.huang@outlook.com>
2025-04-21 19:58:59 +0800

    upd(spare_init): add weeks

--- migrations/0003_create_spares_table.up.sql
+++ migrations/0003_create_spares_table.up.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS spares (
   room_id     INTEGER    NOT NULL
                     REFERENCES rooms(id) ON DELETE CASCADE,
   assignee    INTEGER,                -- 借用人 ID，NULL 表示未借出
-  stamp       INTEGER,                -- 一周里面的 INDEX
+  stamp       INTEGER    NOT NULL,    -- 一周里面的 INDEX
   begin_at    TEXT       NOT NULL,    -- 开始时间
   end_at      TEXT       NOT NULL,    -- 结束时间
   week        TEXT       NOT NULL     -- 本条记录所属周编号
